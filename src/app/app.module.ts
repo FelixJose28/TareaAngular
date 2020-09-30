dedf92d916ca7925701c0e7e4024b46c29c1f3aa
Felix Jose <felixjoseynfantequezada@gmail.com>
2020-09-30 12:39:56 -0400

    Esta funcionando Bien

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule, HttpClient} from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GetDemoComponent } from './gets/get-demo/get-demo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GetDemoComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/gets/get-demo/get-demo.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
+import { UserService } from 'src/app/services/user.service';
+
+@Component({
+  selector: 'app-get-demo',
+  templateUrl: './get-demo.component.html',
+  styleUrls: ['./get-demo.component.css']
+})
+export class GetDemoComponent implements OnInit {
+  notFound = false;
+  user: User;
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+  }
+  getUser(userID: string){
+    this.notFound = false;
+    this.user = null;
+
+    this.userService.getUser(userID).subscribe((userFromTheApi: User)=>{
+      this.user = userFromTheApi;
+
+    },(err: any)=>{
+      console.log(err)
+      this.notFound = true;
+    });
+  }
+
+}
